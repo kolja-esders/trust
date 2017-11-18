49e4070ec2a416eb5195009e9877039177e91431
Oruluk <pat.dein@gmail.com>
2017-11-18 16:26:48 +0100

    Created new page describe-yourself as rough draft

---
+++ src/pages/describe-yourself/describe-yourself.ts
@@ -0,0 +1,25 @@
+import { Component } from '@angular/core';
+import { IonicPage, NavController, NavParams } from 'ionic-angular';
+
+/**
+ * Generated class for the DescribeYourselfPage page.
+ *
+ * See https://ionicframework.com/docs/components/#navigation for more info on
+ * Ionic pages and navigation.
+ */
+
+@IonicPage()
+@Component({
+  selector: 'page-describe-yourself',
+  templateUrl: 'describe-yourself.html',
+})
+export class DescribeYourselfPage {
+
+  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  }
+
+  ionViewDidLoad() {
+    console.log('ionViewDidLoad DescribeYourselfPage');
+  }
+
+}
