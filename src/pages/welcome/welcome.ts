9ae6dcebea846b7fb1ddeeb5f28031026023d302
Alexander Schäfer <alexander.schaefer@uni-duesseldorf.de>
2017-11-18 15:49:56 +0100

    Implement v1 of speech recognition based on ionic plugin

--- src/pages/welcome/welcome.ts
+++ src/pages/welcome/welcome.ts
@@ -1,5 +1,8 @@
 import { Component } from '@angular/core';
 import { IonicPage, NavController } from 'ionic-angular';
+import { Platform } from 'ionic-angular';
+import { SpeechRecognition } from '@ionic-native/speech-recognition';
+import { ChangeDetectorRef } from '@angular/core';
 
 /**
  * The Welcome Page is a splash page that quickly describes the app,
@@ -14,10 +17,54 @@ import { IonicPage, NavController } from 'ionic-angular';
 })
 export class WelcomePage {
 
-  constructor(public navCtrl: NavController) { }
+  matches: String[];
+  isRecording = false;
+  isDemoMode = false;
 
-  login() {
-    this.navCtrl.push('LoginPage');
+  constructor(
+    public navCtrl: NavController,
+    private speechRecognition: SpeechRecognition,
+    private plt: Platform,
+    private cd: ChangeDetectorRef) {
+
+  }
+
+  ionViewDidLoad() {
+    this.getPermission();
+  }
+
+  isIos() {
+    return this.plt.is('ios');
+  }
+
+  stopListening() {
+    this.speechRecognition.stopListening().then(() => {
+      this.isRecording = false;
+    });
+  }
+
+  getPermission() {
+    this.speechRecognition.hasPermission()
+      .then((hasPermission: boolean) => {
+        if (!hasPermission) {
+          this.speechRecognition.requestPermission();
+        }
+      })
+      .catch( () => {
+        console.log("In Demo Mode");
+        this.isDemoMode = true;
+      });
+  }
+
+  startListening() {
+    let options = {
+      language: 'en-US' // TODO: This should be arbitrary...
+    };
+    this.speechRecognition.startListening().subscribe(matches => {
+      this.matches = matches;
+      this.cd.detectChanges();
+    });
+    this.isRecording = true;
   }
 
   signup() {
