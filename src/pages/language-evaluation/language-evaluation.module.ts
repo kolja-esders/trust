04d70ec83b604c8810f51c20ee6029a1713c3bd9
Kolja Esders <kolja.esders@gmail.com>
2017-11-18 18:03:07 +0100

    Some placeholder content for lang. eval.

--- src/pages/language-evaluation/language-evaluation.module.ts
+++ src/pages/language-evaluation/language-evaluation.module.ts
@@ -1,5 +1,6 @@
 import { NgModule } from '@angular/core';
 import { IonicPageModule } from 'ionic-angular';
+import { TranslateModule } from '@ngx-translate/core';
 import { LanguageEvaluationPage } from './language-evaluation';
 
 @NgModule({
@@ -8,6 +9,7 @@ import { LanguageEvaluationPage } from './language-evaluation';
   ],
   imports: [
     IonicPageModule.forChild(LanguageEvaluationPage),
+    TranslateModule.forChild()
   ],
 })
 export class LanguageEvaluationPageModule {}
