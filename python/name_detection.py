f9ac32d89b9c096de2705d998935dbe66888314e
Debjit Paul <deb_4u22@yahoo.com>
2017-11-19 12:38:29 +0100

    Add files via upload

--- python/name_detection.py
+++ python/name_detection.py
@@ -11,15 +11,27 @@ import argparse
 def read_file(text):
      s=['Syria','Jordan','Turkey','Pakistan','Lebanon','Iran','Ethiopia','Kenya','Uganda','Democratic Republic of Congo','Chad','Afghanistan','Lake Chad Basin','South Sudan','Somalia','Ghana'] 
      #text='Mein Name ist Thomas Paul und ich bin 22 Jahre alt Kolkata is where I live.'
-     #file3=open(text,'r')
-     #lines=file3.readlines()
+     #file3=open(text,'r') 
+     #lines=file3.readlines()  
      lines=text
+     lines=str(lines)
      nlp = spacy.load('xx')
      sent = nlp(lines)
      age=''
-     
+     h=''
+
+###Hobbies
+     hobbies=['learning','learn','drawing','draw','cooking','cook','reading','read','gardening','hike','hiking','dance','dancing','play','playing','football','cricket','camping','shopping','shop','photography']
+     for i in range(len(hobbies)):        
+        if hobbies[i].lower() in lines.lower():
+           if hobbies[i]=='football':
+                  
+                  h=hobbies[i]+' '+'playing' 
+                  print(h)
+           else:
+                  h=h+','+hobbies[i]
 ## Age of the person
-     for c in range(len(lines)) :
+     for c in range(len(lines)):
         if lines[c].isdigit() and lines[c+1].isdigit():
            age=lines[c]+lines[c+1]  
            break
@@ -45,15 +57,17 @@ def read_file(text):
      for i in range(len(s)):
          if s[i] in lines:
             place=s[i]
+                 
             break        
-     print(json.dumps({'name': name, 'age': age,'place': place}))
+     print(json.dumps({'name': name,'age': age,'place': place,'hobbies': h}))
+          
 
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("jsonfile", help=".json file containing the input text", nargs='?')
     args = parser.parse_args()
-    read_file(args.jsonfile)
+    read_file(args.jsonfile)   
 
 if __name__ == '__main__':
     main()
