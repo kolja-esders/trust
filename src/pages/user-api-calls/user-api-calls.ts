542e879f6af3b651747d3cb6970757c7e8578abd
Alexander Schäfer <alexander.schaefer@uni-duesseldorf.de>
2017-11-19 11:41:48 +0100

    Implemented pushUser API call

--- src/pages/user-api-calls/user-api-calls.ts
+++ src/pages/user-api-calls/user-api-calls.ts
@@ -1,5 +1,7 @@
-import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import {Component} from '@angular/core';
+import {IonicPage, NavController, NavParams} from 'ionic-angular';
+import {Api} from '../../providers/api/api';
+import {Storage} from '@ionic/storage';
 
 /**
  * Generated class for the UserApiCallsPage page.
@@ -15,7 +17,83 @@ import { IonicPage, NavController, NavParams } from 'ionic-angular';
 })
 export class UserApiCallsPage {
 
-  constructor(public navCtrl: NavController, public navParams: NavParams) {
+  username: string = "";
+  country: string = "";
+  email: string = "";
+  skills: string = "";
+
+  constructor(public navCtrl: NavController, public navParams: NavParams, private api: Api, private storage: Storage) {
+    this.api = api;
+  }
+
+  pushUser() {
+
+    this.storage.get('user-id').then( (val) => {
+
+      let userId = null;
+      let isUpdate = true;
+      console.log("Value from storage", val);
+      if (!val) {
+        console.log("No update, create new user");
+        isUpdate = false;
+      }
+      else {
+        userId = val;
+        console.log("User available - update");
+        console.log("userId is ", userId);
+      }
+
+      console.log(this.username);
+      console.log(this.country);
+      console.log(this.email);
+      console.log(this.skills.split(',').map(function (e) {
+        return e.trim();
+      }));
+
+      if (isUpdate) {
+        console.log("userId is ", userId);
+        this.api.updateUser(
+          userId,
+          this.username,
+          this.country,
+          this.email,
+          this.skills.split(',').map(function (e) {
+            return e.trim();
+          })
+        )
+          .subscribe((response: any) => {
+            console.log(response);
+          });
+      }
+      else {
+        this.api.createNewUser(
+          this.username,
+          this.country,
+          this.email,
+          this.skills.split(',').map(function (e) {
+            return e.trim();
+          })
+        )
+          .subscribe((response: any) => {
+            console.log(response);
+            this.storage.set('user-id', response);
+          });
+      }
+
+    });
+
+  }
+
+  clearStorage() {
+    console.log('Clear storage');
+    this.storage.clear();
+    this.storage.remove('user-id');
+  }
+
+  printId() {
+    this.storage.get('user-id').then( (val) => {
+      console.log(val);
+    })
   }
 
   ionViewDidLoad() {
